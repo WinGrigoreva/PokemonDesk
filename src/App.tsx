ea6df37e8fa97412796a60e2d3bf0d16fff35c11
WiNGrigoreva <kriviktoria@gmail.com>
2020-11-02 15:50:29 +0300

    homework-3: problems solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import cn from 'classnames';
+import cn from 'classnames'; // библиотека для замены конкатенации классов
 import s from './App.module.scss';
 
 const App: React.FC = () => {
   return (
-    // cn (библиотека для замены конкатенации классов) используем для записи вместо {s.header + ' color'}>
+    // cn используем для записи вместо {s.header + ' color'}>
     <div className={s.container}>
       <h1 className={cn(s.header, ' color')}>Hello, my dear frend!</h1>
     </div>
