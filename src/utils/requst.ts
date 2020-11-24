f64ac3a231b0f39a7cd43ada8a06d1591def33c5
WiNGrigoreva <kriviktoria@gmail.com>
2020-11-25 00:19:05 +0300

    Типизация getData и debounce

--- src/utils/requst.ts
+++ src/utils/requst.ts
@@ -1,7 +1,7 @@
 import Url from 'url';
 import getUrlWithParamConfig from './getUrlWithParamConfig';
 
-async function req(endpoint: string, query: object) {
+async function req<T>(endpoint: string, query: object): Promise<T> {
   const uri = Url.format(getUrlWithParamConfig(endpoint, query));
   const result = await fetch(uri).then((res) => res.json());
   return result;
